8ebfdad7936335c73b56d5c8c9c2cfa96c649745
Neil <neilmonastiri@gmail.com>
2022-11-24 11:23:40 +0100

    first commit after repo name change

---
+++ README.md
@@ -0,0 +1,3 @@
+# neilmonastiri.github.io
+Special Thanks to @rammcodes for creating and making available a simple portfolio website
+template for developers, on which I based my personal website on.
---
+++ index.js
@@ -0,0 +1,54 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+// //Hide nav bar when scrolling down
+// const nav = document.querySelector(".header");
+// let lastScrollY = window.scrollY;
+
+// window.addEventListener("scroll", () => {
+//   if (lastScrollY < window.scrollY) {
+//     // console.log("scrolling down");
+//     nav.classList.add("nav--hidden");
+//   } else {
+//     // console.log("scrolling up");
+//     nav.classList.remove("nav--hidden");
+//   }
+
+//   lastScrollY = window.scrollY;
+// })
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
